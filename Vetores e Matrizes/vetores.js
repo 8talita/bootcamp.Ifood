421db9947a81be7fd45210d047bd8b9a76dd179b
Talita Priscila Farias Santos <119720253+8talita@users.noreply.github.com>
2023-10-28 20:11:03 -0300

    commit.vetores

---
+++ Vetores e Matrizes/vetores.js
@@ -0,0 +1,19 @@
+// let pokemon = "pikachu"
+let pokemon2 = "charmander"
+let pokemon3 = "bulbasaur"
+
+//pode ser em armário (vetor), ao invés da representação anterior..
+let pokemon = ["pikachu", "charmander", "bulbasaur"]
+console.log(pokemon)
+
+//se quiser escolher um dos pokemons..exemplo posição 0
+
+console.log(pokemon [0])
+
+//para remover o último elemento - array(matrizes-métodos)
+
+pokemon.pop()
+
+//para remover o primeiro elemento de uma array
+
+pokemon.shift()

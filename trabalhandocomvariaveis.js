9cf8f5cb5adb8ce824e40c7dd552f52d501f1d99
Talita Priscila Farias Santos <119720253+8talita@users.noreply.github.com>
2023-10-28 13:59:21 -0300

    commit.var

---
+++ trabalhandocomvariaveis.js
@@ -0,0 +1,25 @@
+//Saída
+
+console.log("Digite o nome do seu jogador")
+
+//declarar uma variável(quando o nome precisa alterar)
+
+let nickname = "Talitinha"
+
+//Concatenando uma mensagem fixa + uma variável
+
+console.log("Bem vindo " +  nickname)
+
+//Declarar uma constante(Quando é um conteúdo repetitivo, base de mensagem...de link), quando não se pode atribuir um outro valor para ele
+
+
+const notificacao = "Pokemon Go diz:"
+
+console.log("Pokemon Go diz: tem um novo pokemon na região")
+console.log("Pokemon Go diz: você foi derrotado por um líder")
+
+
+let poteCafe = "café pilão"
+let poteAcucar = "Açúcar cristal"
+let poteBiscoito = "Biscoito Maizena"
+

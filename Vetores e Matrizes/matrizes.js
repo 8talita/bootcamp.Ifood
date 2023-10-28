562c70d7e88478a0e4312f22467f2cf147fb5f07
Talita Priscila Farias Santos <119720253+8talita@users.noreply.github.com>
2023-10-28 20:26:29 -0300

    commit.matrizes

---
+++ Vetores e Matrizes/matrizes.js
@@ -0,0 +1,9 @@
+let nomesPokemon = ["pikachu", "charmander", "bulbasaur"]
+let timePokemon = [
+    ["pikachu", "M", "level 1"],
+    ["charmander", "F", "level 4"]
+]
+console.log(timePokemon[0]) //neste caso só teremos a informação da linha do pikachu por ele estar na posição 0
+
+
+

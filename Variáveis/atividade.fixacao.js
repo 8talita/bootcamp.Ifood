abc3a443a814858f6601554fe8fa3befff27052c
Talita Priscila Farias Santos <119720253+8talita@users.noreply.github.com>
2023-10-28 15:13:22 -0300

    commit.fixacao

---
+++ Variáveis/atividade.fixacao.js
@@ -0,0 +1,20 @@
+//Exemplo
+
+let nomePessoa = "Lindinete"
+let idade = 71
+let numeroTelefone = "79-900000000"
+let cpf = "000.000.000.00"
+let endereco = "Rua do pé separado, numero 111, bairro Dos vivos"
+let recebePensao = true
+
+//Atividade consiste em apresentar variáveis de alguma situação ou produto
+
+
+
+let nomeProduto = "Sapato"
+let nomeMarca = "Dakota"
+let numeroProduto = "34"
+let precoProduto = 25
+let lancamento = true
+
+

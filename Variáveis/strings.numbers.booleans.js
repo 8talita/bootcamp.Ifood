1a0a78555984109f3c7e70963c5a67c76df7f9cb
Talita Priscila Farias Santos <119720253+8talita@users.noreply.github.com>
2023-10-28 14:48:42 -0300

    terceiro.commit

---
+++ Variáveis/strings.numbers.booleans.js
@@ -0,0 +1,16 @@
+
+
+//Variável string são as que guardam textos
+
+let nomePokemon = "Pikachu"
+let pokemonSexo = "M"
+
+//Variável number são as que armazenam números
+
+let nivelPokemon = 20
+let pontosDeVidaPokemon = 45
+
+//Variável boolean são lógicas que armazenam ligado ou desligado, verdadeiro ou falso
+
+let selecionavel = false
+

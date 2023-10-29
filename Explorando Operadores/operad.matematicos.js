dd984890515472b93937c7251ef986112e4b2b54
Talita Priscila Farias Santos <119720253+8talita@users.noreply.github.com>
2023-10-28 22:37:47 -0300

    commit.oper.matematicos

---
+++ Explorando Operadores/operad.matematicos.js
@@ -0,0 +1,40 @@
+let idade = 30
+
+idade = 30 + 6
+
+console.log(" operação de adição: " + idade)
+
+idade = 30 - 6
+console.log(" operação de subtração: " + idade)
+
+
+let codigoDoProduto = 1023
+let codigoExato = codigoDoProduto - 1000
+   
+
+let primeiroNumero = 1023
+let segundoNumero = 23
+
+console.log(primeiroNumero - segundoNumero)
+
+
+
+let precoProduto = 100.99
+let valorComTaxa = precoProduto * 2
+console.log(valorComTaxa) 
+
+
+let multiplicador = 4
+let multiplicando = 12
+let produto = multiplicador * multiplicando
+console.log("resultado da multiplicação é: " + produto)
+
+multiplicador = 8
+console.log("resultado da multiplicação é: " + produto)
+
+let notaDoMercado = 50
+let pessoasParaDividir = 2
+console.log("operação de divisão: " + notaDoMercado / pessoasParaDividir)
+
+let calculo = 10 % 3
+console.log("operação de módulo: "  + calculo)

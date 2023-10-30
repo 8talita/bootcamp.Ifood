3947fa61816939e15dce75eb236911fd312a13ad
Talita Priscila Farias Santos <119720253+8talita@users.noreply.github.com>
2023-10-30 10:49:32 -0300

    commit.and

---
+++ Explorando Operadores/operadores.logicos.js
@@ -0,0 +1,20 @@
+// AND (&&)
+let idade = 18
+let vistoVerificado = true
+
+let resultado = (idade>= 18) && (vistoVerificado === true)
+
+console.log(resultado)
+
+
+
+let idade1 = 18
+let vistoVerificado1 = false
+
+let resultado1 = (idade>= 18) && (vistoVerificado1 === true)
+console.log(resultado)
+
+let moedasColetadas = 100
+let item = "estrela"
+let resultado2 = (moedasColetadas >= 100) && (item === "Estrela")
+console.log(resultado2)

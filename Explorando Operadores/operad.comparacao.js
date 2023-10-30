f27258261b169339fb0fc85acfaee382ec395bec
Talita Priscila Farias Santos <119720253+8talita@users.noreply.github.com>
2023-10-30 07:48:41 -0300

    commitei

---
+++ Explorando Operadores/operad.comparacao.js
@@ -0,0 +1,12 @@
+// = é atribuição
+// == é para comparar valor
+// === é para comparar valor e formato do conteúdo
+// !== é para verificar se algo é diferente ou igual
+// OPERADORES IGUAL OU DIFERENTE
+let marca = "Apple"
+console.log(marca !== "Sansung")
+
+// = é atribuição
+// == é para comparar valor
+// === é para comparar valor e formato do conteúdo
+// !== é para verificar se algo é diferente ou igual
